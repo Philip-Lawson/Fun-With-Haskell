266ce231458c3e50ac4aecc82552bd2aacb36861
Philip-Lawson <plawson03@qub.ac.uk>
2015-08-21 10:32:28 +0100

    Percolation Kata
    The solution to the percolation kata at the last Functional Kats meetup.

---
+++ Kata/Percolation.hs
@@ -0,0 +1,47 @@
+percolateKata :: [[Char]] -> Bool
+percolateKata [[]]   = True
+percolateKata (x:xs) = percolateKata' filledList
+                        where filledList = [(fillWithWater x)] ++ xs
+
+percolateKata' :: [[Char]] -> Bool
+percolateKata' [[]]     = True
+percolateKata' [list]   = containsWater list
+percolateKata' (x:y:xs) | (containsWater trickledList) == True = percolateKata' ([trickledList] ++ xs)                          
+                        | otherwise = False
+                           where trickledList = (spreadWaterLeft . spreadWaterRight) (trickleWaterDown x y)
+
+containsWater :: [Char] -> Bool
+containsWater []     = False
+containsWater (x:xs) | x == 'W' = True
+                     | otherwise = containsWater xs  
+
+fillWithWater :: [Char] -> [Char]
+fillWithWater []     = []
+fillWithWater (x:xs) | x == '0'  = "W" ++ fillWithWater xs
+                     | otherwise = [x] ++ fillWithWater xs  
+
+trickleWaterDown :: [Char] -> [Char] -> [Char]
+trickleWaterDown [] _          = []
+trickleWaterDown _ []          = []
+trickleWaterDown (x:xs) (y:ys) | x == 'W' && y == '0' = [x] ++ trickleWaterDown xs ys
+                               | otherwise            = [y] ++ trickleWaterDown xs ys
+                      
+spreadWaterLeft :: [Char] -> [Char]
+spreadWaterLeft []   = []
+spreadWaterLeft [a]  = [a]
+spreadWaterLeft list = (reverse . spreadWaterRight . reverse) list
+
+spreadWaterRight :: [Char] -> [Char]
+spreadWaterRight []       = []
+spreadWaterRight [a]      = [a]
+spreadWaterRight (x:y:xs) | x == 'W' && y == '0' = [x] ++ spreadWaterRight (x:xs)
+                          | otherwise            = [x] ++ spreadWaterRight (y:xs)  
+
+
+
+
+
+
+
+
+
